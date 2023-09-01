ae351ce1d45fb16bb2aa49847362eff6cf6cfc0f
kai <cgk0910@gmail.com>
2023-09-01 09:20:14 +0800

    cars index f1 f2 f3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('car.index');
+})->name('cars.index');
+
+Route::get('/f1', function () {
+    return view('car.f1');
+})->name('cars.f1');
+
+Route::get('/f2', function () {
+    return view('car.f2');
+})->name('cars.f2');
+
+Route::get('/f3', function () {
+    return view('car.f3');
+})->name('cars.f3');
+
